20ec3b7785e8e7b149c9b9876c240ab135eed068
jh <jonghun2931@gmail.com>
2019-04-17 19:28:07 +0900

    0412 Exercise1.h Exercise1.cpp LogicalExercise1.cpp PhysicalExercise1.cpp
    so 파일 생성 그러나 오류
    SubArray operator 참조하여 작성 중.df

--- src/query/ops/Exercise1/PhysicalExercise1.cpp
+++ src/query/ops/Exercise1/PhysicalExercise1.cpp
@@ -94,82 +94,77 @@ namespace scidb {
 
         std::shared_ptr<Array> execute(std::vector< std::shared_ptr<Array> >& inputArrays, std::shared_ptr<Query> query) override
         {
-            LOG4CXX_DEBUG(logger,"PhysicalExercise1::execute called");
+            //LOG4CXX_DEBUG(logger,"PhysicalExercise1::execute called");
             SCIDB_ASSERT(inputArrays.size() == 1); // or assert(inputArrays.size() ==1);
             SCIDB_ASSERT(_schema.getResidency()->isEqual(inputArrays[0]->getArrayDesc().getResidency()));
             //autochunk interval이면 그 interval을 instance에서 확인하고 설정한다.
-            LOG4CXX_DEBUG(logger,"Check or update Intervals");
             checkOrUpdateIntervals(_schema, inputArrays[0]);
+            //LOG4CXX_DEBUG(logger,"Check or update Intervals");
 
-            LOG4CXX_DEBUG(logger,"input array and desc");
             //random access으로 Iterator
             std::shared_ptr<Array> inputArray = ensureRandomAccess(inputArrays[0], query);
             ArrayDesc const& inDesc = inputArray->getArrayDesc();
+            LOG4CXX_DEBUG(logger,"input array and desc");
 
-            LOG4CXX_DEBUG(logger,"input array dimensions");
             Dimensions const& srcDims = inDesc.getDimensions();
             size_t nDims = srcDims.size();
+            LOG4CXX_DEBUG(logger,"input array dimensions");
 
-            LOG4CXX_DEBUG(logger,"for output array  attribute, we choose one of the input array's attribute");
             Attributes outputAttr;
             AttributeID attributeID = ((std::shared_ptr<OperatorParamPhysicalExpression>&)_parameters[nDims * 2])->getExpression()->evaluate().getUint32();
-            outputAttr.push_back(AttributeDesc( attributeID, "attributeName", TID_DOUBLE, 0, CompressorType::NONE));
+            outputAttr.push_back(AttributeDesc( attributeID, "attr1", TID_DOUBLE, 0, CompressorType::NONE));
             outputAttr = addEmptyTagAttribute(outputAttr);
 
-            /*// input AttrID and low and high position of subarray
-            Coordinates lowPos = getWindowStart(inDesc);
-            Coordinates highPos = getWindowEnd(inDesc);
+            LOG4CXX_DEBUG(logger,"for output array  attribute, we choose one of the input array's attribute");
 
-            //low가 high보다 클 경우 empty array를 반환한다.
-            for(size_t i=0; i<nDims; i++)
-            {
-                if (lowPos[i] > highPos[i]) {
-                    return std::shared_ptr<Array>(new MemArray(_schema,query));
-                }
-
-            }*/
 
             LOG4CXX_DEBUG(logger,"set starting cell and ending cell about subarray window ");
-            vector<int64_t> startingCell;
-            vector<int64_t> endingCell;
+            Coordinates startingCell = getWindowStart(inDesc);;
+            Coordinates endingCell= getWindowEnd(inDesc);
             vector<double> outputs;
 
-            LOG4CXX_DEBUG(logger,"make startingcell");
+
+/*            LOG4CXX_DEBUG(logger,"make startingcell");
             for (size_t i = 0; i < nDims; i ++) {
                 int64_t dimension =
                         ((std::shared_ptr<OperatorParamPhysicalExpression>&)_parameters[i])->getExpression()->evaluate().getInt64();
                 startingCell.push_back(dimension);
-                LOG4CXX_DEBUG(logger,"starting cell : "<<dimension);
             }
+
+            ;
             LOG4CXX_DEBUG(logger,"make endingcell ");
             for (size_t i = nDims; i < nDims * 2; i ++) {
                 int64_t dimension =
                         ((std::shared_ptr<OperatorParamPhysicalExpression>&)_parameters[i])->getExpression()->evaluate().getInt64();
                 endingCell.push_back(dimension);
-                LOG4CXX_DEBUG(logger,"ending cell : "<<dimension);
-            }
-
+            }*/
 
             std::shared_ptr<ConstArrayIterator> arrayIterator = inputArray->getConstIterator(attributeID);
-            LOG4CXX_DEBUG(logger,"array Iterator start");
-
-
-
+            LOG4CXX_DEBUG(logger,"input array Iterator start");
             while (!arrayIterator->end())
-            {//현재 instance가 가지고 있는 chunk들을 순회(현재는 전체 array가 하나의 chunk로 구성되어 있음)
+            {
+                //현재 instance가 가지고 있는 chunk들을 순회(현재는 전체 array가 하나의 chunk로 구성되어 있음)
                 ConstChunk const &chunk = arrayIterator->getChunk();//arrayIterator로부터 chunk를 읽어옴
                 LOG4CXX_DEBUG(logger,"Chunk size :"<< chunk.getSize());
-                if(chunk.contains(startingCell,false))
-                {
-                    std::shared_ptr<ConstChunkIterator> chunkIterator = chunk.getConstIterator();
-                    chunkIterator->setPosition(startingCell);
-
-                    while(!chunkIterator->end() || (chunkIterator->getPosition() < endingCell) ) {
+                std::shared_ptr<ConstChunkIterator> chunkIterator = chunk.getConstIterator();
+                //chunkIterator->setPosition(startingCell);
+                while(!chunkIterator->end()) {
+                    bool checkboundary = true;
+                    for(size_t i =0 ; i <nDims; i++)
+                    {
+                        if(chunkIterator->getPosition()[i] > endingCell[i])
+                        {
+                            checkboundary &= false;
+                        }
+                    }
+                    if(checkboundary)
+                    {
                         double cell = chunkIterator->getItem().getDouble();//read cell
-                        outputs.push_back(cell);
                         LOG4CXX_DEBUG(logger, "contain cells value : " << cell << "   position" << chunkIterator->getPosition());
+                        outputs.push_back(cell);
                         ++(*chunkIterator);
                     }
+
                 }
                 ++(*arrayIterator);
             }
@@ -178,56 +173,57 @@ namespace scidb {
             /*결과 반환*/
             if (query->getInstanceID() == 0)
             {
-                LOG4CXX_DEBUG(logger,"Exercise1Array start ");
+                LOG4CXX_DEBUG(logger,"Exercise1Array start");
                 return Exercise1Array(startingCell,endingCell,outputs,query);
             }
             else
             {
                 /* Instance 0번이 아닐 경우 empty array 반환*/
-                LOG4CXX_DEBUG(logger,"Exercise1Array start ");
+                LOG4CXX_DEBUG(logger,"Exercise1Array start");
                 return std::shared_ptr<Array>(new MemArray(_schema, query));
             }
         }
+        std::shared_ptr<Array> Exercise1Array (Coordinates startingCell,Coordinates endingCell,vector<double> attributeValues,std::shared_ptr<Query>& query)
+        {
 
-        std::shared_ptr<Array> Exercise1Array
-            (Coordinates startingCell,Coordinates endingCell,vector<double> attributeValues,std::shared_ptr<Query>& query){
             LOG4CXX_INFO(logger,"Exercise1Array called");
             std::shared_ptr<Array> outputArray(new MemArray(_schema, query));
             //Coordinates startingPosition(1, query->getInstanceID());
-
             //output attribute
+
+
             std::shared_ptr<ArrayIterator> outputArrayIter = outputArray->getIterator(0);
             LOG4CXX_INFO(logger,"ArrayIterator outputArrayIter start");
+
             std::shared_ptr<ChunkIterator> outputChunkIter = outputArrayIter->newChunk(startingCell).getIterator(query,ChunkIterator::SEQUENTIAL_WRITE);
             LOG4CXX_INFO(logger,"ChunkIterator outputChunkIter start");
+
             outputChunkIter->setPosition(startingCell);
-
-
             while(!outputArrayIter->end()){
-                while(!outputChunkIter->end()){
+                while(!outputChunkIter->end())
+                {
 
-                    for(size_t i = 0; i <attributeValues.size() ;i++)
+                    LOG4CXX_INFO(logger,"attributesvalues size "<< attributeValues.size());
+
+                    Coordinates co = outputChunkIter->getPosition();
+                    int64_t index =0;
+                    for(size_t i =0 ; i <2 ;i++ )
                     {
-                        Value value;
-                        value.setDouble(attributeValues[i]);
-                        outputChunkIter->writeItem(value);
-                        ++(*outputChunkIter);
+                        index = co[i];
                     }
-                    outputChunkIter->flush();
+                        Value value;
+                        value.setDouble(attributeValues[index]);
+                        LOG4CXX_INFO(logger,"output value" << value.getUint32());
+                        outputChunkIter->writeItem(value);
+
+                        LOG4CXX_INFO(logger,"write value" );
+
+
+                    ++(*outputChunkIter);
                 }
+                outputChunkIter->flush();
+                ++(*outputArrayIter);
             }
-            //"Example" of writing cells
-
-         /*   Value value;
-            value.setDouble(attributeValues[0]);
-            outputChunkIter->writeItem(value);
-
-            ++(*outputChunkIter); // move to the next cell
-
-            value.setDouble(attributeValues[1]);
-            outputChunkIter->writeItem(value);
-
-            outputChunkIter->flush(); // After completing a chunk, you have to flush.*/
 
             LOG4CXX_INFO(logger,"Exercise1Array finished");
 
